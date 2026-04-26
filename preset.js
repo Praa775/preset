0ea6c2e3dc34cb065b5736529476527e3d5215ff
Praa775 <yudhamation@gmail.com>
2026-04-26 10:57:14 +0700

    Create preset.js

---
+++ preset.js
@@ -0,0 +1,170 @@
+[
+  {
+    "title": "PRESET NOMOR 123",
+    "video": "https://files.catbox.moe/u1w6jc.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/duawhjeFox-1046f8b34ef0a387",
+    "xml": "https://drive.google.com/file/d/1CtbvcAK2JvixGoNoGqimR50HZdf2kUSh/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSDVAeeP9/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+  }, 
+ {
+  "title": "PRESET NOMOR 110",
+    "video": "https://files.catbox.moe/gwlzrr.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/OyA8rHXNPN-d045b10f9ce297ce",
+    "xml": "https://drive.google.com/file/d/1LhaRf52aFez5PDiRQ4URarPy7ItznOT6/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSyhxePS6/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+  }, 
+ {
+  "title": "PRESET NOMOR 113",
+    "video": "https://files.catbox.moe/7e89kc.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/gStgoFf9WZ-74f341ae6b6ebf80",
+    "xml": "https://drive.google.com/file/d/1i3yzCXgxmkKZGaR4NDg2qPOIw5_1HOKE/view?usp=drivesdk",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 128",
+    "video": "https://files.catbox.moe/ihpt39.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/NVIdyXm6ts-aa9a78f73b172bf0",
+    "xml": "https://drive.google.com/file/d/1PNtplK615v1DF6VWxfjuKf_gupkHTnWe/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSDrsEhGc/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 116",
+    "video": "https://files.catbox.moe/7oqnkc.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/MTOVi0CAKy-529779494dd9b1e3",
+    "xml": "https://drive.google.com/file/d/1m7k4QPK6uYwKeAeNeSJLX9r5NOiJT_gx/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSyhQA1cM/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 119",
+    "video": "https://files.catbox.moe/7851a2.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/cqj3B5f7bX-d1900cd43b6cda6b",
+    "xml": "https://drive.google.com/file/d/1Fjf8rk92J_n003esea-Ayywtu-R5eD7V/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSD2ragmK/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 115",
+    "video": "https://files.catbox.moe/9y7lt0.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/cvEw0RppPS-fb7c606a315fca6f",
+    "xml": "https://drive.google.com/file/d/14jiLqt64auprmKIe-99k2bHsi2-RSqlh/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSD6AWcaK/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 126",
+    "video": "https://files.catbox.moe/kjposd.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/E1nrVRKlYW-0e2ae26e77f58d2b",
+    "xml": "https://drive.google.com/file/d/1VLchbN9HrTtqdTVo1bb-GAkGmsowvYBV/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSD6wfm4x/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 129",
+    "video": "https://files.catbox.moe/158vpn.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/tsLr1MSGrf-8237bf74bdcaed06",
+    "xml": "https://drive.google.com/file/d/1aNxjIBDlgn9OxTROzW4UBVx-gy3W3ISj/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSDrthy15/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 130 v1",
+    "video": "https://files.catbox.moe/22n6xv.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/ZXlyH9QTpH-115362b80fecfb6d",
+    "xml": "https://drive.google.com/file/d/1pudVC16VUrfNLxRu5mWkCFoJYzsODly1/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSDDSvnjW/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 130 v2",
+    "video": "https://files.catbox.moe/9qop93.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/JGkgsnps0W-382eb572a2270378",
+    "xml": "https://drive.google.com/file/d/1KbL2aiW26vk00BmpNGSHl_H9g64VDGzw/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSDDSvnjW/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 134",
+    "video": "https://files.catbox.moe/g0buj0.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/xQSBdKkR2U-6e557336774b4079",
+    "xml": "https://drive.google.com/file/d/1GkV2mLz9f-kFx1nQuwYa9a0tfo4fBkgG/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSDVBw2yv/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 133",
+    "video": "https://files.catbox.moe/59mcmo.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/p3jpyB7gP5-22fd9b8af500a180",
+    "xml": "https://drive.google.com/file/d/12WIXXp2fuRQtAfbXnD6NV2tJK4Kc_Hyc/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSD5j4YXE/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 139",
+    "video": "https://files.catbox.moe/wkprnn.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/XbEhDA46Om-1db6d7139b68889e",
+    "xml": "https://drive.google.com/file/d/1tsaNKyv9R3JXTbvA0_kJta_potxxMeLQ/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSDVAeeP9/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 149",
+    "video": "https://files.catbox.moe/4xxgxm.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/JigiCfWDo1-1bcfd0bbfe646d17",
+    "xml": "https://drive.google.com/file/d/1fPDyFYUjAhovdoJzkpE2Ooc7WTo5ojol/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSDcbEaVc/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 159",
+    "video": "https://files.catbox.moe/q51ydn.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/Nslgkm8bI8-b2aa42fd3bc5cb93",
+    "xml": "https://drive.google.com/file/d/1q8WLhe2_V1tncRCDvhy__Iuw0FaUguk1/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSyhKBU1o/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 160",
+    "video": "https://files.catbox.moe/7xcllw.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/5WcDlXvCCO-2a7493cd78a45b91",
+    "xml": "https://drive.google.com/file/d/117cHSkrTLoKzH8G6FFdsLATLNtkB98s8/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSyhErcyu/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 195",
+    "video": "https://files.catbox.moe/tdnjce.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/poGOuJFwaa-ed47d2820d9a3bb1",
+    "xml": "https://drive.google.com/file/d/1I7e3M39VNVAbLgrRzKElbUXBniGhV60s/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSyhEd6A4/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
+      }, 
+ {
+  "title": "PRESET NOMOR 199",
+    "video": "https://files.catbox.moe/4aa3xq.mp4",
+    "link5mb": "https://alightcreative.com/am/share/u/3nPmmRagBKY3Bm6v7IYps4p7wBC3/p/DZXYBMoxtd-7232f7714a5095cc",
+    "xml": "https://drive.google.com/file/d/1pyzVXZz29aYUQwjZ7vK2PNdF3LWphesX/view?usp=drivesdk",
+    "sound": "https://vt.tiktok.com/ZSyhECFMo/",
+    "grup": "https://chat.whatsapp.com/E5Cmcq0pPg27M80zTuBNT9?mode=wwt",
+    "note": "OPEN JASA EDIT & ALIGHT MOTION PREM 1 TAHUN 10K PRIVATE DM 085643623053"
